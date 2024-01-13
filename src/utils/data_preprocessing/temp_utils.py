4a5fae47b26179b1068c86d89336a9f638f3550e
cretaceousmart <cretaceous1020@gmail.com>
2024-01-13 16:53:49 +0000

    Improve: Train Kernel(RBF) SVM

--- src/utils/data_preprocessing/temp_utils.py
+++ src/utils/data_preprocessing/temp_utils.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from PIL import Image
 import os 
 from tqdm import tqdm
+import math
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -18,30 +19,38 @@ def rename_edf(raw):
     """
     reanme the channel name of raw signal
     """
-    EEG_channels_name = [chanel for chanel in raw.ch_names if "EEG" in chanel]
-    assert len(EEG_channels_name) == 16, f"EEG channels number is not 16 but {len(EEG_channels_name)}"
-    raw.pick(EEG_channels_name)
-    # rename the channel name
     channel_mapping = {
-        'EEG C3-Ref': 'C3',
-        'EEG C4-Ref': 'C4',
-        'EEG O1-Ref': 'O1',
-        'EEG O2-Ref': 'O2',
-        'EEG Fp1-Ref': 'Fp1',
-        'EEG Fp2-Ref': 'Fp2',
-        'EEG T3-Ref': 'T7',  # T3 在标准10-20系统中通常对应于T7
-        'EEG T4-Ref': 'T8',  # T4 在标准10-20系统中通常对应于T8
-        'EEG F3-Ref': 'F3',
-        'EEG F4-Ref': 'F4',
-        'EEG P3-Ref': 'P3',
-        'EEG P4-Ref': 'P4',
-        'EEG F7-Ref': 'F7',
-        'EEG F8-Ref': 'F8',
-        'EEG T5-Ref': 'P7',  # T5 在标准10-20系统中通常对应于P7
-        'EEG T6-Ref': 'P8'   # T6 在标准10-20系统中通常对应于P8
-    }
+            'EEG C3-Ref': 'C3',
+            'EEG C4-Ref': 'C4',
+            'EEG O1-Ref': 'O1',
+            'EEG O2-Ref': 'O2',
+            'EEG Fp1-Ref': 'Fp1',
+            'EEG Fp2-Ref': 'Fp2',
+            'EEG T3-Ref': 'T7',  # T3 在标准10-20系统中通常对应于T7
+            'EEG T4-Ref': 'T8',  # T4 在标准10-20系统中通常对应于T8
+            'EEG F3-Ref': 'F3',
+            'EEG F4-Ref': 'F4',
+            'EEG P3-Ref': 'P3',
+            'EEG P4-Ref': 'P4',
+            'EEG F7-Ref': 'F7',
+            'EEG F8-Ref': 'F8',
+            'EEG T5-Ref': 'P7',  # T5 在标准10-20系统中通常对应于P7
+            'EEG T6-Ref': 'P8'   # T6 在标准10-20系统中通常对应于P8
+        }
 
+    # EEG_channels_name = [chanel for chanel in raw.ch_names if "EEG" in chanel]
+    # print(f"Jie Log: len(EEG_channels_name): {len(EEG_channels_name)}, unique_name: {len(set(EEG_channels_name))}")
+    # print(f"Jie Log: EEG_channels_name: {EEG_channels_name}")
+    # assert len(EEG_channels_name) == 16, f"EEG channels number is not 16 but {len(EEG_channels_name)}"
+    
+    # pick the target channel
+    target_EEG_channel = list(channel_mapping.keys())
+    raw.pick(target_EEG_channel)
+    
+    # rename the channel name
     raw.rename_channels(channel_mapping)
+
+    
     
     montage = mne.channels.make_standard_montage('standard_1020')
     raw.set_montage(montage, match_case=False, on_missing='warn')
@@ -81,20 +90,31 @@ def obtain_multi_topomap(raw, picks, info, eeg_file_name, fig_size=128, is_energ
 
     current_path = os.path.abspath('')
     root_dir = os.path.dirname(os.path.dirname(current_path))
-    output_dir = os.path.join(root_dir, 'EEG_DL','src','data', 'output_image', eeg_file_name)
+    output_dir = os.path.join(root_dir, 'EEG_DL_CLASSIFIER','src','data', 'output_image', eeg_file_name)
 
     print(f"Jie Log: output_dir: {output_dir}")
     os.makedirs(output_dir, exist_ok=True)
-    sample_rate = raw.info['sfreq']
+    sample_rate = int(raw.info['sfreq'])
 
     index = 3600 if not is_test else 10
-    for i in tqdm(range(index), desc=f"Obtain topomap for {eeg_file_name}"):
+
+    # # 动态计算循环次数以覆盖所有样本
+    # total_samples = len(raw.times)
+    # index = total_samples // sample_rate
+    # print(f"Jie Log: sample_rate: {sample_rate}, total_samples: {total_samples}, index: {index}")
+    # if (total_samples % sample_rate != 0) and not is_test:
+    #     index += 1  # 如果有剩余样本，增加一个循环来覆盖它们
+    
+    for i in tqdm(range(0,index), desc=f"Obtain topomap for {eeg_file_name}"):
         start_time = int(i * sample_rate)  # 将秒转换为样本数
         end_time = int((i + 1) * sample_rate)  # 将秒转换为样本数
+
+        # print(f"Jie Log: start_time: {start_time}, end_time: {end_time}, sample_rate: {sample_rate}, int(start_time/sample_rate): {int(start_time/sample_rate)}")
         fig = obtain_single_topomap(raw, picks, info, start_time=start_time, end_time=end_time, is_energy=is_energy)
 
         output_path = os.path.join(output_dir, f"{eeg_file_name}_{int(start_time/sample_rate)}.png")
-        temp_path = f'../../../data/output_image/temp.png'     
+        temp_path = f'../src/data/output_image/temp.png'     
+
         fig.savefig(temp_path, dpi=100)  # 保存原始图形
         with Image.open(temp_path) as _img:
             _img_resized = _img.resize((fig_size, fig_size), Image.Resampling.LANCZOS)
@@ -147,7 +167,46 @@ def prepare_data(eeg_file_name, handle_data_imbalance = False, is_test = True):
     
 
 
-    
+def prepare_data_from_multi_file(eeg_file_names, handle_data_imbalance = False, is_test = True):
+    all_images = []
+    all_labels = []
+
+    for eeg_file_name in eeg_file_names:
+        # 读取每个图像的标签
+        with pd.ExcelFile(rf'../src/data/test_data/{eeg_file_name}/{eeg_file_name}.xlsx') as xls:
+            label_df = pd.read_excel(xls, 'Sheet1', header=None)
+            label_df.rename(columns={0: 'label'}, inplace=True)
+
+        if is_test:
+            label_df = label_df[:100]
+
+        # 读取图像数据
+        image_folder = rf'../src/data/output_image/{eeg_file_name}/'
+        label_size = 3600 if not is_test else 100 
+
+        for image_id in range(label_size):
+            image_path = os.path.join(image_folder, f'{eeg_file_name}_{image_id}.png')
+            with Image.open(image_path) as img:
+                img_gray = img.convert('L')  # 转换为灰度图像
+                img_array = np.array(img_gray).flatten()  # 展平为一维向量
+                all_images.append(img_array)
+
+        # 将标签追加到总列表
+        all_labels.extend(label_df['label'].tolist())
+
+    # 将图像数据转换为 numpy 数组
+    all_images_np = np.array(all_images)
+    # 将标签转换为 numpy 数组
+    all_labels_np = np.array(all_labels)
+
+    # 划分训练集和测试集
+    print(f"Jie log: all_images_np.shape: {all_images_np.shape}, all_labels_np.shape: {all_labels_np.shape}")
+    if handle_data_imbalance:
+        X_train, X_test, y_train, y_test = train_test_split(all_images_np, all_labels_np, test_size=0.3, random_state=42, stratify=all_labels_np)
+    else:
+        X_train, X_test, y_train, y_test = train_test_split(all_images_np, all_labels_np, test_size=0.3, random_state=42)
+
+    return X_train, X_test, y_train, y_test
 
 
     
